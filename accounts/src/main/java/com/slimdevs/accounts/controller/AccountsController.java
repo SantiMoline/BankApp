3a3e982e6b9080f873f91bc0e04bcdcb01d479c0
Santiago Moline <molinesanti@gmail.com>
2024-01-08 21:00:31 -0300

    Added findByMobileNumber method to customerRepository interface. Final implementation of the createAccount method in AccountController.

--- accounts/src/main/java/com/slimdevs/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/slimdevs/accounts/controller/AccountsController.java
@@ -11,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.slimdevs.accounts.constants.AccountConstants;
 import com.slimdevs.accounts.dto.CustomerDto;
 import com.slimdevs.accounts.dto.ResponseDto;
+import com.slimdevs.accounts.service.IAccountService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
     
+    IAccountService iAccountService;
+
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountService.createAccount(customerDto);
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
--- accounts/src/main/java/com/slimdevs/accounts/repository/CustomerRepository.java
+++ accounts/src/main/java/com/slimdevs/accounts/repository/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.slimdevs.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.slimdevs.accounts.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
