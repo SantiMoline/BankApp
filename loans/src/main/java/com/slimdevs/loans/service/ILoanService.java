f2df676e810ed63617cc8a0bd5effb06e48bc6f1
Santiago Moline <molinesanti@gmail.com>
2024-01-17 20:53:43 -0300

    ILoanService interface and LoanServiceImplementation finished with CRUD operations.

--- loans/src/main/java/com/slimdevs/loans/service/ILoanService.java
+++ loans/src/main/java/com/slimdevs/loans/service/ILoanService.java
@@ -1,5 +1,34 @@
 package com.slimdevs.loans.service;
 
+import com.slimdevs.loans.dto.LoanDto;
+
 public interface ILoanService {
     
+    /**
+     * 
+     * @param mobileNumber - Customer's mobile number.
+     */
+    void createLoan(String mobileNumber);
+
+    /**
+     * 
+     * @param mobileNumber - Customer's mobile number.
+     * @return Loan's details based on a given mobile number.
+     */
+    LoanDto fetchLoan(String mobileNumber);
+
+
+    /**
+     * 
+     * @param loanDto - LoanDto Object
+     * @return boolean indicating if the update of the loan's details was successful or not.
+     */
+    boolean updateLoan(LoanDto loanDto);
+
+    /**
+     * 
+     * @param mobileNumber - Input mobile number.
+     * @return boolean indicating if the deletion of the loan's details was successful or not.
+     */
+    boolean deleteLoan(String mobileNumber);
 }
--- loans/src/main/java/com/slimdevs/loans/service/impl/LoanServiceImpl.java
+++ loans/src/main/java/com/slimdevs/loans/service/impl/LoanServiceImpl.java
@@ -1,7 +1,17 @@
 package com.slimdevs.loans.service.impl;
 
+import java.util.Optional;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
+import com.slimdevs.loans.constant.LoanConstants;
+import com.slimdevs.loans.dto.LoanDto;
+import com.slimdevs.loans.entity.Loan;
+import com.slimdevs.loans.exception.LoanAlreadyExistsException;
+import com.slimdevs.loans.exception.ResourceNotFoundException;
+import com.slimdevs.loans.mapper.LoanMapper;
+import com.slimdevs.loans.repository.LoanRepository;
 import com.slimdevs.loans.service.ILoanService;
 
 import lombok.AllArgsConstructor;
@@ -10,5 +20,60 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class LoanServiceImpl implements ILoanService  {
     
-    
+    private LoanRepository loanRepository;
+
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loan> optionalLoan = loanRepository.findByMobileNumber(mobileNumber);
+        if (optionalLoan.isPresent()) {
+            throw new LoanAlreadyExistsException("There's already a loan registered with given mobile number: " + mobileNumber);
+        }
+        loanRepository.save(createNewLoan(mobileNumber));
+        
+    }
+
+    @Override
+    public LoanDto fetchLoan(String mobileNumber) {
+        Loan loan = loanRepository.findByMobileNumber(mobileNumber).orElseThrow(
+            () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber.toString()) 
+            );
+
+        return LoanMapper.mapToLoanDto(loan, new LoanDto());
+    }
+
+    @Override
+    public boolean updateLoan(LoanDto loanDto) {
+        Loan loan = loanRepository.findByLoanNumber(loanDto.getLoanNumber()).orElseThrow(
+            () -> new ResourceNotFoundException("Loan", "loanNumber", loanDto.getLoanNumber())
+        );
+
+        LoanMapper.mapToLoan(loanDto, loan);
+        loanRepository.save(loan);
+        return true;
+    }
+
+    @Override
+    public boolean deleteLoan(String mobileNumber) {
+        Loan loan = loanRepository.findByMobileNumber(mobileNumber).orElseThrow(
+            () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber.toString())
+        );
+
+        loanRepository.deleteById(loan.getLoanId());
+        return true;
+    }
+
+    private Loan createNewLoan(String mobileNumber) {
+        Loan newLoan = new Loan();
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoanConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoanConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoanConstants.NEW_LOAN_LIMIT);
+
+        return newLoan;
+    }
+
+
 }
