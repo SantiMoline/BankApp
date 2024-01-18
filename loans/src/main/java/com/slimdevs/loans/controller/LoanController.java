deadc82822f65871d897745843a47364df442799
Santiago Moline <molinesanti@gmail.com>
2024-01-18 02:24:42 -0300

    Genereted CRUD endpoints inside LoanController Class.

--- loans/src/main/java/com/slimdevs/loans/controller/LoanController.java
+++ loans/src/main/java/com/slimdevs/loans/controller/LoanController.java
@@ -1,9 +1,20 @@
 package com.slimdevs.loans.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.slimdevs.loans.constant.LoanConstants;
+import com.slimdevs.loans.dto.LoanDto;
+import com.slimdevs.loans.dto.ResponseDto;
 import com.slimdevs.loans.service.ILoanService;
 
 import lombok.AllArgsConstructor;
@@ -13,7 +24,51 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class LoanController {
     
-    ILoanService loanService;
+    ILoanService iLoanService;
 
-    
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createLoan(@RequestParam String mobileNumber) {
+        iLoanService.createLoan(mobileNumber);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(new ResponseDto(LoanConstants.STATUS_201, LoanConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<LoanDto> fetchLoanDetails(@RequestParam String mobileNumber) {
+        LoanDto loanDto = iLoanService.fetchLoan(mobileNumber);
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(loanDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateLoanDetails(@RequestBody LoanDto loanDto) {
+        boolean isUpdate = iLoanService.updateLoan(loanDto);
+        if (isUpdate) {
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(LoanConstants.STATUS_200, LoanConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(LoanConstants.STATUS_417, LoanConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteLoanDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = iLoanService.deleteLoan(mobileNumber);
+        if (isDeleted) {
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(LoanConstants.STATUS_200, LoanConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(LoanConstants.STATUS_417, LoanConstants.MESSAGE_417_DELETE));
+        }
+    }
+
+
 }
