10b5dc2b194594aa7b44ebbf546f8f6dd4a17f85
Santiago Moline <molinesanti@gmail.com>
2024-01-18 20:39:34 -0300

    Added Audit annotations to BaseEntity's fields. Implemented audit logic inside audit package (hardcoding filler users information until spring security is implemented. EnableJpaAuditing to app's main class.

--- loans/src/main/java/com/slimdevs/loans/entity/BaseEntity.java
+++ loans/src/main/java/com/slimdevs/loans/entity/BaseEntity.java
@@ -2,7 +2,14 @@ package com.slimdevs.loans.entity;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,17 +17,22 @@ import lombok.ToString;
 
 @MappedSuperclass
 @Getter @Setter @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
     
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 }
