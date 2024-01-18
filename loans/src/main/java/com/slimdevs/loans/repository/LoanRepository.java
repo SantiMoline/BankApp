87c3cef66d6ae90e0add383f8b1e1befff623446
Santiago Moline <molinesanti@gmail.com>
2024-01-17 21:02:06 -0300

    Added findByMobileNumber and findByLoanNumber methods to repository interface.

--- loans/src/main/java/com/slimdevs/loans/repository/LoanRepository.java
+++ loans/src/main/java/com/slimdevs/loans/repository/LoanRepository.java
@@ -1,9 +1,13 @@
 package com.slimdevs.loans.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.slimdevs.loans.entity.Loan;
 
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
     
+    Optional<Loan> findByMobileNumber(String mobileNumber);
+    Optional<Loan> findByLoanNumber(String loanNumber);
 }
