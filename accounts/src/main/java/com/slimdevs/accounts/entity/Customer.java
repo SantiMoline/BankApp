65fe49dc5e6f313a2b96157968e509f9c5eecfcb
Santiago Moline <molinesanti@gmail.com>
2024-02-09 17:38:59 -0300

    Changed ID  Strategy GeneretionType.IDENTITY inside Customer Class.

--- accounts/src/main/java/com/slimdevs/accounts/entity/Customer.java
+++ accounts/src/main/java/com/slimdevs/accounts/entity/Customer.java
@@ -1,6 +1,5 @@
 package com.slimdevs.accounts.entity;
 
-import org.hibernate.annotations.GenericGenerator;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,8 +14,7 @@ import lombok.*;
 public class Customer extends BaseEntity {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long customerId;
     private String name;
     private String email;
